c8a5114713c2508a7929289d6bbab1833dd6ebf9
Andrew Cheong <slackwing@gmail.com>
2025-02-23 12:01:50 -0500

    feat: first article

--- foundry/website/html/awcml-macros/macro.vspace.js
+++ foundry/website/html/awcml-macros/macro.vspace.js
@@ -16,13 +16,7 @@
 function __define_macro() {
   return /* async */ function() {
     let config = arguments[0] ??= {};
-    let url = arguments[1] ??= "placeholder.png";
-    let width = arguments[2] ??= "640";
-    let caption = arguments[3] ??= "";
-    // TODO: Deprecate caption in favor of separate macro.
-    if (caption !== "") {
-      caption = `<p style="margin: 0; font-size: 1rem; width: ${width}px;"><span>${caption}</span></p>`;
-    }
-    return `<center><img src="/.staging/shared/assets/${url}" style="max-width: ${width}px;"/>${caption}</center>`;
+    let height = arguments[1] ??= ""
+    return `<div style="height: ${height};"></div>`;
   }
 }
