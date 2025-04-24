050e9b29cd9cbb7aaabeae703712882617e710f8
Andrew Cheong <slackwing@gmail.com>
2025-04-23 20:09:13 -0400

    feat: non-erroring buy/sell form

--- 0d.algo-sabin/lib/Order.js
+++ 0d.algo-sabin/lib/Order.js
@@ -1,5 +1,10 @@
+export const BookType = {
+    L2: 'L2',
+    PAPER: 'PAPER'
+};
+
 export class Order {
-    constructor(type, id, side, price, quantity, timestamp) {
+    constructor(type, id, side, price, quantity, timestamp, book_type) {
         this.type = type;
         this.id = id;
         this.side = side;
@@ -7,6 +12,7 @@ export class Order {
         this.quantity = quantity;
         this.timestamp = timestamp;
         this.filled_qty = 0;
+        this.book_type = book_type;
     }
 
     get remaining_qty() {
