201c105d2a1a4f039cff463d8641f744f169398a
Andrew Cheong <slackwing@gmail.com>
2025-05-03 14:24:48 -0400

    wip

--- 0e.algo-cyan/src/lib/derived/World.ts
+++ 0e.algo-cyan/src/lib/derived/World.ts
@@ -4,17 +4,17 @@ import { PubSub } from '../infra/PubSub';
 import { Trade } from '../base/Trade';
 
 export class World {
-    private _combinedBook: OrderBook;
+    public combinedBook: OrderBook;
     protected onTrade(trade: Trade): void {
         // Does nothing. Override in subclass.
     }
 
     constructor() {
-        this._combinedBook = new OrderBook();
+        this.combinedBook = new OrderBook();
     }
 
     subscribeOrderFeed(orderFeed: PubSub<Order>): void {
-        orderFeed.subscribe(this._combinedBook.onOrder);
+        orderFeed.subscribe(this.combinedBook.onOrder);
     }
 
     subscribeTradeFeed(tradeFeed: PubSub<Trade>): void {
