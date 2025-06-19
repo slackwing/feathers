4b338d0d2814d4c8295a029a4c4117266c6e172d
Andrew Cheong <slackwing@gmail.com>
2025-06-19 15:53:52 -0400

    chore: missed file

--- 0e.algo-cyan/src/lib/derived/ExchangeRecorder.ts
+++ 0e.algo-cyan/src/lib/derived/ExchangeRecorder.ts
@@ -1,8 +1,37 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as readline from 'readline';
-import { ExchangeConfig, ExchangeMessage } from './types';
 import { PATHS } from '@/config';
+
+export interface ExchangeMessage {
+  type: string;
+  channel: string;
+  timestamp?: string;
+}
+
+export interface ExchangeConfig {
+  exchange: string;
+  channels: string[];
+  pair: string;
+  fileSizeBytes: number;
+}
+
+export interface ExchangeSubscription {
+  type: 'subscribe' | 'unsubscribe';
+  channel: string;
+  product_ids?: string[];
+}
+
+export interface ExchangeConnection {
+  connect: (callbacks: {
+    onMessage?: (data: ExchangeMessage) => void;
+    onError?: (error: Event) => void;
+    onOpen?: () => void;
+  }) => void;
+  disconnect: () => void;
+  subscribe: (channel: string, productIds?: string[]) => void;
+  unsubscribe: (channel: string, productIds?: string[]) => void;
+} 
 
 export abstract class ExchangeRecorder {
   protected buffer: ExchangeMessage[] = [];
